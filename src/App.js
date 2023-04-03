63045c187b48b6d52ed81a12e6cec036c19779ee
ngkao <ng.kao.ca@gmail.com>
2023-04-03 14:38:07 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios"
+import "./App.scss"
 
 function App() {
+
+  const handleClick = () => {
+    console.log("CHECKOUT clicked")
+
+    axios.get("http://localhost:8080")
+        .then(response => console.log(response))
+        .catch(error => console.log(error))
+
+    fetch('http://localhost:8080/create-checkout-session', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        items: [
+          { id: 1, quantity: 2},
+          { id: 2, quantity: 1}
+        ]
+      })
+    }).then(res => {
+      if (res.ok) return res.json()
+      return res.json().then(json => Promise.reject(json))
+    }).then(({url}) => {
+      // window.location = url
+      console.log(url)
+    }).catch(e => {
+      console.error(e.error)
+    })
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="checkout">
+        <button 
+          onClick={handleClick}
+          className="checkout__btn">CHECKOUT</button>
     </div>
   );
 }
