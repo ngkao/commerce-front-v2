962261b4308a51d372ac43d29caa7fc2e20ad0f6
ngkao <ng.kao.ca@gmail.com>
2023-04-05 12:45:59 -0400

    add total cart add logic

--- src/App.js
+++ src/App.js
@@ -4,17 +4,19 @@ import "./App.scss"
 import PaySummary from "./pages/PaySummary/PaySummary";
 import { useEffect, useState } from "react";
 import InventoryList from "./pages/InventoryList/InventoryList";
+import { v4 as uuidv4 } from 'uuid';
 
 const REACT_APP_SERVER_URL = process.env.REACT_APP_SERVER_URL;
 
 function App() {
 
 const [productList, setProductList] = useState();
+const myId = uuidv4();
 
 useEffect(() => {
   axios.get(`${REACT_APP_SERVER_URL}/products`)
       .then((res) => {
-        console.log(res.data)
+        // console.log(res.data)
         setProductList(res.data)
       })
       .catch((err) => console.log(err))
@@ -54,31 +56,80 @@ useEffect(() => {
   }
 
   const [sumCart, setSumCart] = useState([]);
-  const cart = [];
 
+  const startStr = sessionStorage.getItem("myCart");
+  let startCart = JSON.parse(startStr);
+  console.log(startCart)
+  const cart = startCart;
+  
   // Total Cart
   const totalCart = (product) => {
-    console.log("Before Push Total Cart",cart)
+    // setClickTrigger(true)
+    // console.log("Before Push Total Cart",cart)
     cart.push(product)
-    console.log("After Push Total Cart",cart)
+    // console.log("After Push Total Cart",cart)
+    // console.log(cart)
+    // setSumCart(cart)
+    // console.log("TOTAL CART inside Func",sumCart)
+    // return cart
+    // setClickTrigger(false)
+    // renderTotalCart()
+    const num = clickTrigger;
+    setClickTrigger(num +1)
+    console.log(clickTrigger)
+    sessionStorage.setItem("myCart", JSON.stringify(cart))
 }
+
+const [cartSession, setCartSession] = useState([]);
+const [clickTrigger, setClickTrigger] = useState(0);
+
+
+
+// const renderTotalCart = () => {
+//   const str = sessionStorage.getItem("myCart");
+//   let myCart = JSON.parse(str);
+//   setCartSession(myCart)
+//   console.log("My Cart from sessionStorage",myCart)
+// }
+
+// const str = sessionStorage.getItem("myCart");
+// let myCart = JSON.parse(str);
+// console.log(myCart)
+
+useEffect(() => {
+  // renderTotalCart()
+  const str = sessionStorage.getItem("myCart");
+  let myCart = JSON.parse(str);
+  setCartSession(myCart)
+  console.log("My Cart from sessionStorage",myCart)
+},[clickTrigger])
+
+
+
 
   return (
     <>
         {productList? 
         productList.map((product) => (
+          <>
             <InventoryList 
               key={product.id}
               product={product}
               onClick={handleClick}
               totalCart={totalCart}
             /> 
+          </>
         ))
-
-      
-
-      
       : <p>Loading</p>}
+
+        <div>
+          <p>TOTAL PAY CART</p>
+          {/* {console.log("Before MAP", cartSession)} */}
+          {cartSession.map((cartItem) => (
+            <p key={myId}>cartItem {cartItem.product_name}</p>
+          ))}
+        </div>
+
       <PaySummary onClick={handleClick}/>
     {/* // <BrowserRouter>
     //   <Routes>
