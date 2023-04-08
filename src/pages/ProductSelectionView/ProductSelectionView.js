cccfd1072e13d972cd38af01b33ee975e50269a7
ngkao <ng.kao.ca@gmail.com>
2023-04-08 19:20:05 -0400

    add animation for buttons

--- src/pages/ProductSelectionView/ProductSelectionView.js
+++ src/pages/ProductSelectionView/ProductSelectionView.js
@@ -2,31 +2,33 @@ import React from 'react';
 import InventoryList from '../InventoryList/InventoryList';
 import { Link } from 'react-router-dom';
 import "./ProductSelectionView.scss"
-import Carousel from 'react-multi-carousel';
+// import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
+
+import Carousel from 'react-grid-carousel'
 
 const ProductSelectionView = ({productList, key, product, onClick,totalCart,removeFromCart}) => {
     console.log(productList)
 
-    const responsive = {
-        superLargeDesktop: {
-          // the naming can be any, depends on you.
-          breakpoint: { max: 4000, min: 3000 },
-          items: 5
-        },
-        desktop: {
-          breakpoint: { max: 3000, min: 1024 },
-          items: 3
-        },
-        tablet: {
-          breakpoint: { max: 1024, min: 464 },
-          items: 2
-        },
-        mobile: {
-          breakpoint: { max: 464, min: 0 },
-          items: 1
-        }
-      };
+    // const responsive = {
+    //     superLargeDesktop: {
+    //       // the naming can be any, depends on you.
+    //       breakpoint: { max: 3000, min: 1280 },
+    //       items: 5
+    //     },
+    //     desktop: {
+    //       breakpoint: { max: 1280, min: 768 },
+    //       items: 3
+    //     },
+    //     tablet: {
+    //       breakpoint: { max: 768, min: 464 },
+    //       items: 2
+    //     },
+    //     mobile: {
+    //       breakpoint: { max: 464, min: 0 },
+    //       items: 1
+    //     }
+    //   };
 
     return (
         <section className="product">
@@ -35,8 +37,8 @@ const ProductSelectionView = ({productList, key, product, onClick,totalCart,remo
                 <Link className="product__add" to="/products/add">Add Product</Link>
             </div>
 
-            {/* <div className="product__ctr"> */}
-                {/* <div className="product__list"> */}
+            <div className="product__ctr">
+                <div className="product__list">
                     {/* {productList? 
                     productList.map((product) => (
                         <InventoryList 
@@ -47,27 +49,34 @@ const ProductSelectionView = ({productList, key, product, onClick,totalCart,remo
                             removeFromCart={removeFromCart}
                         />
                     )): <p>Loading</p>} */}
-                <Carousel className="carousel" responsive={responsive}>
-                    {productList? 
-                    productList.map((product) => (
-                        <InventoryList 
-                            key={product.id}
-                            product={product}
-                            onClick={onClick}
-                            totalCart={totalCart}
-                            removeFromCart={removeFromCart}
-                            productList={productList}
-                            // className="inventory__item"
-                        />
-                    ))
-                    : <p>Loading</p>}
-                    
-
+                <Carousel 
+                    className="carousel" 
+                    cols={3} 
+                    rows={3} 
+                    gap={0} 
+                    showDots={true}
+                    loop>
+                        {productList? 
+                        productList.map((product) => (
+                            <Carousel.Item className="test">
+                                <InventoryList 
+                                    key={product.id}
+                                    product={product}
+                                    onClick={onClick}
+                                    totalCart={totalCart}
+                                    removeFromCart={removeFromCart}
+                                    productList={productList}
+                                />
+                            </Carousel.Item>
+                        ))
+                        : <p>Loading</p>}
                 </Carousel>
 
-                {/* </div> */}
 
-            {/* </div> */}
+
+                </div>
+
+            </div>
 
         </section>
     );
