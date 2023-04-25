9ff06243e876ebe142ebd01795dd93261d38cd26
ngkao <ng.kao.ca@gmail.com>
2023-04-25 10:32:17 -0400

    fix the cart item name vs price overlay

--- src/pages/Insights/Insights.js
+++ src/pages/Insights/Insights.js
@@ -1,7 +1,6 @@
 import BarChart from "../../components/BarChart/BarChart";
 import "./Insights.scss"
 import { useEffect, useState } from "react";
-import {Line} from 'react-chartjs-2';
 
 
 const Insights = ({orders}) => {
