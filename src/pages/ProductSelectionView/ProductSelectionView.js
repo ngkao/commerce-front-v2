56abaf7660459db88abe3755344c377a0e47f4c1
ngkao <ng.kao.ca@gmail.com>
2023-04-28 10:33:51 -0400

    remove carousel

--- src/pages/ProductSelectionView/ProductSelectionView.js
+++ src/pages/ProductSelectionView/ProductSelectionView.js
@@ -3,27 +3,61 @@ import InventoryList from '../InventoryList/InventoryList';
 import { Link } from 'react-router-dom';
 import "./ProductSelectionView.scss"
 import 'react-multi-carousel/lib/styles.css';
-import Carousel from 'react-grid-carousel'
+// import Carousel from 'react-grid-carousel';
+import { useEffect } from 'react';
 
-const ProductSelectionView = ({productList, key, product, onClick,totalCart,removeFromCart, cartSession, setCartSession,showQuantity, setShowQuantity,setPreviewCart}) => {
+const ProductSelectionView = ({productList, key, product, onClick,totalCart,removeFromCart, cartSession, setCartSession,showQuantity, setShowQuantity,setPreviewCart,productsSold,setOutOfStockMsg,setShowQR}) => {
+
+    useEffect(() => {
+        if (!productList) {
+            <p>STILL LOADING</p>
+        }
+    },[])
 
     return (
         <section className="product">
-            <div className="product__topbar">
+            {/* <div className="product__topbar">
                 <input className="product__search" placeholder="SEARCH"></input>
                 <Link className="product__add" to="/products/add">Add Product</Link>
-            </div>
+            </div> */}
             <div className="product__ctr">
                 <div className="product__list">
-                    <Carousel 
+                    {/* <Carousel 
                         className="carousel" 
-                        cols={4} 
-                        rows={3} 
+                        // cols={4} 
+                        // rows={3} 
+                        // cols={1} 
+                        responsiveLayout={
+                            [
+                                {
+                                    breakpoint: 767,
+                                    cols: 1,
+                                    rows: 3,
+                                    gap: 0,
+                                    loop: true,
+                                  //   autoplay: 1000
+                                },
+                                {
+                                    breakpoint: 3000,
+                                    cols: 4,
+                                    rows: 3,
+                                    gap: 0,
+                                    loop: true,
+                                  //   autoplay: 1000
+                                }
+                            ]
+                        }
+                        mobileBreakpoint={1}
+                        // scrollSnap={true}
                         gap={0} 
                         showDots={true}
-                        loop>
+                        loop
+                        >
+                            {console.log(productList)}
                             {productList? 
-                            productList.map((product) => (
+                            productList
+                            .sort((a,b) => a.id - b.id)    
+                            .map((product) => (
                                 <Carousel.Item className="test" key={product.id}>
                                     <InventoryList 
                                         key={product.id}
@@ -37,11 +71,14 @@ const ProductSelectionView = ({productList, key, product, onClick,totalCart,remo
                                         showQuantity={showQuantity}
                                         setShowQuantity={setShowQuantity}
                                         setPreviewCart={setPreviewCart}
+                                        productsSold={productsSold}
+                                        setOutOfStockMsg={setOutOfStockMsg}
+                                        setShowQR={setShowQR}
                                     />
                                 </Carousel.Item>
                             ))
                             : <p>Loading</p>}
-                    </Carousel>
+                    </Carousel> */}
                 </div>
             </div>
         </section>
