27d1b0817708a4a82a868b73e97432c0a36ca4cc
ngkao <ng.kao.ca@gmail.com>
2023-05-09 09:11:10 -0400

    adjust the isntruction highlights logic

--- src/pages/Insights/Insights.js
+++ src/pages/Insights/Insights.js
@@ -9,19 +9,14 @@ const Insights = ({orders}) => {
     const currentMonth = today.getMonth() + 1; // getMonth() returns 0-indexed month
     const salesData = Array.from({ length: 31 }, (_, i) => i + 1).fill(0);
 
-    console.log("orders", orders)
-
     orders.forEach(order => {
     const orderMonth = parseInt(order.order_date.slice(5, 7)); 
 
-    console.log("OrderMonth", orderMonth)
         if (orderMonth === currentMonth) {
             const day = parseInt(order.order_date.slice(8, 10)) - 1; 
             salesData[day + 1] += parseInt(order.total_sale_amount); 
         }
     });
-
-    console.log("data", salesData)
 
     const [salesChartData, setSalesChartData] = useState(
         {
@@ -62,7 +57,7 @@ const Insights = ({orders}) => {
                 plugins: {
                   legend: {
                     display: true,
-                  },
+                  }                
                 }
               }
         },
--- src/pages/Instruction/Instruction1.js
+++ src/pages/Instruction/Instruction1.js
@@ -10,7 +10,7 @@ const Instruction = () => {
 
     useEffect(() => {
         const instructionCtrs = document.querySelectorAll('.instruction__ctr');
-        if (location.pathname == "/") {
+        if (location.pathname === "/") {
              instructionCtrs.forEach((instructionCtr) => {
                      instructionCtr.classList.add('instruction__ctr--fade-in');
              });
@@ -26,7 +26,7 @@ const Instruction = () => {
     const navItems = document.querySelectorAll('.nav__item');
     const navItemProducts = Array.from(navItems).find(item => item.textContent.trim() === 'Products');
     const navItemInventory= Array.from(navItems).find(item => item.textContent.trim() === 'Inventory');
-    const navItemSales = Array.from(navItems).find(item => item.textContent.trim() === 'Sales');
+    const navItemSales = Array.from(navItems).find(item => item.textContent.trim() === 'Sales History');
     const navItemInsights = Array.from(navItems).find(item => item.textContent.trim() === 'Insights');
     const paysumCoverBottom = document.querySelector('.pay-cover-bottom');
     const paysumCoverTop = document.querySelector('.pay-cover-top');
@@ -34,74 +34,62 @@ const Instruction = () => {
 
     const handleNextCard = () => {
 
-
-
         if(cardNumber < 5) {
 
-            // Nav
+            // const navCoverBottom = document.querySelector('.nav-cover-bottom');
+            // const navCoverTop = document.querySelector('.nav-cover-top');
+            // const paysumCoverBottom = document.querySelector('.pay-cover-bottom');
+            // const paysumCoverTop = document.querySelector('.pay-cover-top');
 
-            // const navContainers = document.querySelectorAll('.nav');
-            // navContainers.forEach((navContainer) => {
-            // navContainer.style.opacity = '0.5';
-            // navContainer.style.zIndex = '999'
-            // });
-
-       
             navCoverBottom.classList.add('nav-cover-bottom-on');
             navCoverTop.classList.add('nav-cover-top-on');
             paysumCoverBottom.classList.add('pay-cover-bottom-on');
             paysumCoverTop.classList.add('pay-cover-top-on');
 
+            // if (navCoverBottom && navCoverTop && navItems && navItemProducts && navItemInventory && navItemSales && navItemInsights & paysumCoverBottom && paysumCoverTop && payItemQR) {
+                
+                if (cardNumber === 0) {
+                 
+                    if (navItemProducts) {
+                        const navItemProducts = Array.from(navItems).find(item => item.textContent.trim() === 'Products');
+                        navItemProducts.style.zIndex = '999'
+                    } 
+                } else {
+                    navItemProducts.style.zIndex = '1'
+                }
 
+                if (cardNumber === 1) {
+                    if (payItemQR) {
+                        payItemQR.style.zIndex = '999'
+                    } 
+                } else {
+                    payItemQR.style.zIndex = '1'
+                }
 
+                if (cardNumber === 2) {
+                    if (navItemInventory) {
+                        navItemInventory.style.zIndex = '999'
+                    } 
+                } else {
+                    navItemInventory.style.zIndex = '1'
+                }
 
+                if (cardNumber === 3) {
+                    if (navItemSales) {
+                        navItemSales.style.zIndex = '999'
+                    } 
+                } else {
+                    navItemSales.style.zIndex = '1'
+                }
 
-
-            if (cardNumber === 0) {
-                if (navItemProducts) {
-                    navItemProducts.style.zIndex = '999'
-                } 
-            } else {
-                navItemProducts.style.zIndex = '1'
-            }
-
-            if (cardNumber === 1) {
-                if (payItemQR) {
-                    payItemQR.style.zIndex = '999'
-                } 
-            } else {
-                payItemQR.style.zIndex = '1'
-            }
-
-            if (cardNumber === 2) {
-                if (navItemInventory) {
-                    navItemInventory.style.zIndex = '999'
-                } 
-            } else {
-                navItemInventory.style.zIndex = '1'
-            }
-
-            if (cardNumber === 3) {
-                if (navItemSales) {
-                    navItemSales.style.zIndex = '999'
-                } 
-            } else {
-                navItemSales.style.zIndex = '1'
-            }
-
-            if (cardNumber === 4) {
-                if (navItemInsights) {
-                    navItemInsights.style.zIndex = '999'
-                } 
-            } else {
-                navItemInsights.style.zIndex = '1'
-            }
-
-            // PaySum
-            // const paysumContainers = document.querySelectorAll('.paysum');
-            // paysumContainers.forEach((paysumContainer) => {
-            //     paysumContainer.style.opacity = '0.5';
-            //     });
+                if (cardNumber === 4) {
+                    if (navItemInsights) {
+                        navItemInsights.style.zIndex = '999'
+                    } 
+                } else {
+                    navItemInsights.style.zIndex = '1'
+                }
+            // }
 
         } else {
             const navs = document.querySelectorAll('.nav');
@@ -130,14 +118,6 @@ const Instruction = () => {
     }
 
     const handleSkip = () => {
-        // const navs = document.querySelectorAll('.nav');
-        // navs.forEach(nav => {
-        // nav.style.opacity = '1';
-        // });
-        // const paysumContainers = document.querySelectorAll('.paysum');
-        // paysumContainers.forEach((paysumContainer) => {
-        //     paysumContainer.style.opacity = '1';
-        //     });
         navItemInsights.style.zIndex = '1';
         navCoverBottom.classList.remove('nav-cover-bottom-on');
         navCoverTop.classList.remove('nav-cover-top-on');
@@ -148,42 +128,41 @@ const Instruction = () => {
 
     return (
         <section className="instruction">
-            {/* <p className="instruction__title">Instruction</p> */}
             <div className={`instruction__ctr`}>
                 <div className="instruction__subctr">
-                    {cardNumber == 0 ? <InstructionCard
+                    {cardNumber === 0 ? <InstructionCard
                         section="Welcome on Board"
-                        text="Snack POS is a commerce POS solution that caters to small brick and mortar retail owners or pop-up shops the following benefits:"
+                        text="Snack POS is a commerce POS solution that caters to small retail owners or pop-up shops with the following benefits:"
                         text1="1) Checkout with QR Codes and Stripe Payments"
                         text2="2) Simple Inventory management"
                         text3="3) Business Performance Insights"
                         className="instruction__subtext--hide"
                     /> : null}
-                    {cardNumber == 1 ? <InstructionCard
+                    {cardNumber === 1 ? <InstructionCard
                         section="Products"
                         text="A merchant selects products that a customer would like to buy."
                         dot1="instruction__dot--selected"
                         className="instruction__subtext--hide"
                     /> : null}
-                    {cardNumber == 2 ? <InstructionCard
+                    {cardNumber === 2 ? <InstructionCard
                         section="Payment"
                         text2="A merchant generates a QR code for a customer payment via Stripe."
                         text3="A customer can use a phone to scan (or click on the QR code)."
                         dot2="instruction__dot--selected"
                     /> : null}
-                    {cardNumber == 3 ? <InstructionCard
+                    {cardNumber === 3 ? <InstructionCard
                         section="Inventory"
                         text="A merchant can view the units sold and the units available in stock."
                         dot3="instruction__dot--selected"
                         className="instruction__subtext--hide"
                     /> : null}
-                    {cardNumber == 4 ? <InstructionCard
+                    {cardNumber === 4 ? <InstructionCard
                         section="Sales"
                         text="A merchant can view detailed sales transaction records."
                         dot4="instruction__dot--selected"
                         className="instruction__subtext--hide"
                     /> : null}
-                    {cardNumber == 5 ? <InstructionCard
+                    {cardNumber === 5 ? <InstructionCard
                         section="Insights"
                         text="A merchant can view business metrics for today and this month."
                         dot5="instruction__dot--selected"
